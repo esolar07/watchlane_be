a324e0996b4f46dcbf9068cab418c76150b79e46
esolar07 <esolar07@gmail.com>
2026-02-16 17:29:42 -0500

    - fixed issue with mismatch in Prisma clients and adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,15 @@
 import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { config } from "../config/env";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const adapter = new PrismaPg({ connectionString: config.databaseUrl });
+
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({ accelerateUrl: config.databaseUrl });
+  globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
