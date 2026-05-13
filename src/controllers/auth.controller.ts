f05dcb341742b493b115714c71ce7df8c85589ad
esolar07 <esolar07@gmail.com>
2026-05-12 22:05:52 -0400

    pushing fix

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -21,10 +21,12 @@ const googleClient = new OAuth2Client(
   config.google.redirectUri
 );
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "lax" as const,
+  secure: isProduction,
+  sameSite: (isProduction ? "none" : "lax") as "none" | "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
